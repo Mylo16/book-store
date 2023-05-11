3f5a0b6b73cb7db2dc9d7c19e657a8f055f322d6
Antwi Eric <ANTWI@DESKTOP-92MFH60.(none)>
2023-05-11 13:22:47 +0000

    finish adding react-redux component

--- src/components/bookCards.js
+++ src/components/bookCards.js
@@ -1,43 +1,44 @@
 import '../App.css';
-import Proptypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/bookSlice';
 
-export default function BookCards({ books }) {
+export default function BookCards() {
+  const dispatch = useDispatch();
+  const books = useSelector((store) => store.book.books);
   return (
     <>
-      {
-        books.map((book) => (
-          <div key={book.id} className="book-container">
-            <div className="card-col1">
-              <p>{book.category}</p>
-              <h1>{book.title}</h1>
-              <p>{book.author}</p>
-              <ul className="features">
-                <li className="feature"><button type="button" className="link">Comments</button></li>
-                <li className="feature"><button type="button" className="link">Remove</button></li>
-                <li className="feature"><button type="button" className="link">Edit</button></li>
-              </ul>
-            </div>
-            <div className="card-col2">
-              <div className="progress" />
-              <div className="progress-info">
-                <p>{book.completion}</p>
-                <p>Completed</p>
+      <div className="bks">
+        {
+          books.map((book) => (
+            <div key={book.item_id} className="book-container">
+              <div className="card-col1">
+                <p>{book.category}</p>
+                <h1>{book.title}</h1>
+                <p>{book.author}</p>
+                <ul className="features">
+                  <li className="feature"><button type="button" className="link">Comments</button></li>
+                  <li className="feature"><button type="button" onClick={() => dispatch(removeBook(book.item_id))} className="link remove">Remove</button></li>
+                  <li className="feature"><button type="button" className="link">Edit</button></li>
+                </ul>
+              </div>
+              <div className="card-col2">
+                <div className="progress" />
+                <div className="progress-info">
+                  <p>80%</p>
+                  <p>Completed</p>
+                </div>
+              </div>
+              <div className="card-col3">
+                <p>CURRENT CHAPTER</p>
+                <p>CHAPTER 3</p>
+                <button type="button" className="update">
+                  UPDATE PROGRESS
+                </button>
               </div>
             </div>
-            <div className="card-col3">
-              <p>CURRENT CHAPTER</p>
-              <p>{book.chapter}</p>
-              <button type="button" className="update">
-                UPDATE PROGRESS
-              </button>
-            </div>
-          </div>
-        ))
-      }
+          ))
+        }
+      </div>
     </>
   );
 }
-
-BookCards.propTypes = {
-  books: Proptypes.arrayOf(Proptypes.shape({})).isRequired,
-};
